b38a22b49e6cd301e6fdf9e2915d1c8eb8c09288
dylanclements7 <dylanclements77@gmail.com>
2025-11-07 14:16:08 -0500

    removed js files, added storyOverview to ai.py and connected to frontend

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.idea, name='idea'),
-    path('draft/', views.draft, name='draft'),
     path('personas/', views.personas, name='personas'),
     path('locations/', views.locations, name='locations'),
     path('scene/', views.scene, name='scene'),
     path('video/', views.video, name='video'),
+    path('draft/', views.draft, name='draft'),
 ]
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
-
+from .logic.ai import storyOverview, characterGenerate, locationGenerate, sceneGenerate, narrationGenerate
+import logging
+logger = logging.getLogger(__name__)
 def idea(request):
     if request.method == 'POST':
         return redirect("draft")
     return render(request, 'main/idea.html')
 
 def draft(request):
-    return render(request, 'main/draft.html')
+    if request.method == 'POST':
+        idea_text = request.POST.get('story_prompt', '')
+        overview = storyOverview(idea_text)
+        logger.warning(f"Overview generated: {overview}")
+        return render(request, 'main/draft.html', {'overview': overview})
+    return redirect("idea")
 
 def personas(request):
     return render(request, 'main/personas.html')
@@ -19,3 +26,21 @@ def scene(request):
 
 def video(request):
     return render(request, 'main/video.html')
+
+
+#def viewGeneratePersonas(request):
+    #if request.method == 'POST':
+        
+
+#def viewGenerateLocations(request):
+    #if request.method == 'POST':
+        
+
+#def viewGenerateScene(request):
+    #if request.method == 'POST':
+        
+
+
+#def viewStoryGenerate(request):
+    #if request.method == 'POST':
+        
